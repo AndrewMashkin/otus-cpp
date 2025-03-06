1d9783c623f3465ce72f5d2c8cfc7912db03590a
Машкин Андрей Игоревич <a.mashkin@stc-spb.ru>
2025-03-06 20:36:19 +0300

    refactor-[]: mv test -> getest_frame_work

---
+++ libs/gtest_framework/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/t1/work/otus/otus-cpp/libs/test
+# Build directory: /home/t1/work/otus/otus-cpp/libs/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
