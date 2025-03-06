51cd1acbab6454397b68ffbb08eba21a083e5772
Машкин Андрей Игоревич <a.mashkin@stc-spb.ru>
2025-03-06 19:39:48 +0300

    refactor-[]: ctest on

--- main/unittest.cmake
+++ main/unittest.cmake
@@ -1,4 +1,3 @@
-
 file(GLOB SRC ${CMAKE_CURRENT_SOURCE_DIR}/test/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/test/*.h)
 
 set(TEST_PRJ_NAME version_api)
@@ -9,6 +8,7 @@ target_link_libraries(${TEST_PRJ_NAME}_t gtest_main version_lib)
 set_target_properties(${TEST_PRJ_NAME}_t PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${TEST_OUTPUT_PATH})
 set_target_properties(${TEST_PRJ_NAME}_t PROPERTIES RUNTIME_OUTPUT_DIRECTORY_RELEASE ${TEST_OUTPUT_PATH})
 add_test(NAME ${TEST_PRJ_NAME}_t COMMAND ${TEST_PRJ_NAME}_t WORKING_DIRECTORY ${TEST_OUTPUT_PATH})
+
 
 
 
