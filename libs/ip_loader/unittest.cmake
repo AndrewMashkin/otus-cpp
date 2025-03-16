e926729a6f6f9f3f2f32f10733ded902c511232f
Машкин Андрей Игоревич <a.mashkin@stc-spb.ru>
2025-03-16 21:02:56 +0300

    feat-[]: add iploader libs and emty file for next dev

---
+++ libs/ip_loader/unittest.cmake
@@ -0,0 +1,26 @@
+cmake_minimum_required(VERSION 3.0)
+
+if(UNITTEST )
+	if(NOT TEST_OUTPUT_PATH)
+		set(TEST_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test)
+	endif()
+
+	file(GLOB SRC ${CMAKE_CURRENT_SOURCE_DIR}/test/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/test/*.h)
+
+	add_executable(${PROJECT_NAME}_t ${SRC})
+
+	target_link_libraries(${PROJECT_NAME}_t ${PROJECT_NAME} gtest_main)
+        set_target_properties(${PROJECT_NAME}_t PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${TEST_OUTPUT_PATH})
+        set_target_properties(${PROJECT_NAME}_t PROPERTIES RUNTIME_OUTPUT_DIRECTORY_RELEASE ${TEST_OUTPUT_PATH})
+        add_test(NAME ${PROJECT_NAME}_t COMMAND ${PROJECT_NAME}_t WORKING_DIRECTORY ${TEST_OUTPUT_PATH})
+
+        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/test/config/conf.cmake)
+                message("configure test " ${PROJECT_NAME}_t)
+                include(${CMAKE_CURRENT_SOURCE_DIR}/test/config/conf.cmake)
+        endif()
+
+endif()
+
+
+
+
